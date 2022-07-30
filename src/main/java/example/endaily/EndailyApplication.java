9a631a1c788c53e50a0067df70ddf4f2e87ec9f0
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2022-07-30 15:47:00 +0900

    initialize project

---
+++ src/main/java/example/endaily/EndailyApplication.java
@@ -0,0 +1,13 @@
+package example.endaily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndailyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EndailyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/endaily/EndailyApplicationTests.java
@@ -0,0 +1,13 @@
+package example.endaily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndailyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
