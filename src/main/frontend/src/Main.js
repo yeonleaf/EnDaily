07ee4a4a96335669fbfd911e089c28d3b13a3d4e
yeonleaf <91470133+yeonleaf@users.noreply.github.com>
2022-08-05 18:52:06 +0900

    #1 P9 Diary Component 틀 잡기 (remove 버튼은 아직 포함 안함)

--- src/main/frontend/src/Main.js
+++ src/main/frontend/src/Main.js
@@ -1,9 +1,18 @@
 import React, {Component} from "react";
-
+import {Route, Link} from "react-router-dom";
+import DiaryBox from "./DiaryBox";
+import SearchBox from "./SearchBox";
 const Main = () => {
     return (
-        <div>메인 페이지</div>
+        <div>
+            <DiaryBox />
+            <SearchBox />
+        </div>
     )
+}
+
+function SearchBtn(props) {
+    return <button></button>
 }
 
 export default Main;
